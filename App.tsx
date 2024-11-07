6cd9c3b3b577a02f25d5fd7fcb52c543229ace44
Jovan BIENVENU <bienvenujovan@gmail.com>
2024-11-07 14:59:21 +0100

    feat: add React Native Paper

--- App.tsx
+++ App.tsx
@@ -2,16 +2,25 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './src/screens/HomeScreen';
 import BoxingScreen from './src/screens/BoxingScreen';
+import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+import customColors from "./src/constants/colors";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const theme = {
+    ...DefaultTheme,
+    ...customColors
+  };
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Boxing" component={BoxingScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Boxing" component={BoxingScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
 
 const HomeScreen = () => {
   return (
     <View>
       <Text>Home</Text>
+      <Button mode='contained'>Paper</Button>
     </View>
   );
 };
